a7b6929f517b0192fb11d27906234573fae5c344
knock5 <miftachulhd5@gmail.com>
2023-09-01 16:15:10 +0700

    add: mongodb connection in server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,22 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 
+// express app
 const app = express();
 
+// connect to mongo
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  family: 4,
+});
+mongoose.connection
+  .on('open', () => console.log('DATABASE CONNECTED'))
+  .on('close', () => console.log('DATABASE CLOSE'))
+  .on('error', (error) => console.log('DATABASE ERROR', error));
+
+// routes
 app.get('/', (req, res) => {
   res.json({ message: 'Hello there!' });
 });
